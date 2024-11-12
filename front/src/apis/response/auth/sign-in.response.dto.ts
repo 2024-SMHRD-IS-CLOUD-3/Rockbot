02ca3620de846b4a3f384a971276cf42955085ec
박경록 <pp24346627@gmail.com>
2024-11-12 09:28:35 +0900

    Delete front directory

--- front/src/apis/response/auth/sign-in.response.dto.ts
+++
@@ -1,7 +0,0 @@
-import ResponseDto from "../response.dto";
-
-export default interface SignInResponseDto extends ResponseDto {
-    token: string;
-    expirationTime: number;
-    userId: string;
-}
--- front/src/types/enums/index.ts
+++
@@ -1,7 +0,0 @@
-import ResponseCode from "./response-code.enums";
-import ResponseMessage from "./response-message.enums";
-
-export {
-    ResponseCode,
-    ResponseMessage
-}
